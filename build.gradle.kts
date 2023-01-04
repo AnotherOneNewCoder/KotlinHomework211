8e63bc2335f20fb19df8795fc6156a961a2f0d7c
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2023-01-04 16:19:03 +0300

    set build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    id ("java")
+    id ("org.jetbrains.kotlin.jvm") version ("1.5.20")
 }
 
 group = "org.example"
@@ -12,9 +14,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    //testImplementation(kotlin("test"))
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation ("junit:junit:4.13.2")
 }
-
+//
 tasks.test {
     useJUnitPlatform()
 }
@@ -22,3 +26,12 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+
+//compileKotlin {
+//    kotlinOptions.jvmTarget = "1.8"
+//}
+//compileTestKotlin {
+//    kotlinOptions.jvmTarget = "1.8"
+//}
+
