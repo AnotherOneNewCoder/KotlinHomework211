bc299379ded3c7a4d019b0aaa39822857111b9a1
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2023-01-05 03:04:15 +0300

    added masterCardFunc in test

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -10,12 +10,26 @@ class MainKtTest {
     fun calculateVK() {
         val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.DOWN
-        val  currentMonthTransfer = 30_000_00.00
+        val  currentMonthTransfer = 10_000_00.00
         val amountTransfer = 13_900_00.00
 
-        var result = calculate(currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val result = calculate(currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
         assertEquals("сумма перевода составит - 13900.0 без комиссии", result)
 
 
     }
+    @Test
+    fun calculateMaster() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 500_000_00.00
+        val amountTransfer = 83_900_00.00
+        val card = "MasterCard"
+
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        assertEquals("сумма перевода составит - 84423,4руб, комиссия - 523,4 руб", result)
+
+
+    }
+
 }
