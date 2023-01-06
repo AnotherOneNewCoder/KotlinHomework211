42483f4f32fcba9f317819fccd3fe2cb6f434584
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2023-01-06 23:25:36 +0300

    added more funcs tin test 81% cover

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,37 +3,46 @@ import java.util.*
 import java.math.RoundingMode
 import java.text.DecimalFormat
 fun main() {
+    val dayCardLimitTransfer = 150_000_00.00
+    val monthCardLimitTransfer = 600_000_00.00
+    val beforeFee = 75_000_00.00
 
     val amount = Scanner(System.`in`).useLocale(Locale.US) // использую точку как delimiter, а не запятую
 
     while (true){
         println("Введите сумму перевода в рублях: ")
         val userInput = (amount.nextDouble()*100); // сразу перевожу в копейки.
-        println(calculate("MasterCard", amountTransfer = userInput))
-        println(calculate("Мир",amountTransfer = userInput))
+        println(calculate("MasterCard", amountTransfer = userInput, dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee))
+        println(calculate("Мир",amountTransfer = userInput, dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer))
         println(calculate(amountTransfer = userInput))
 
     }
 
 }
-fun calculate(typeCard :String = "VK Pay", currentMonthTransfer: Double = 0.0, amountTransfer : Double): String {
+fun calculate(typeCard :String = "VK Pay", currentMonthTransfer: Double = 0.0, amountTransfer : Double, dayCardLimitTransfer : Double = 15_000_00.00,
+                monthCardLimitTransfer: Double = 40_000_00.00, beforeFee: Double = 0.0): String {
     // для округления
     val df = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.DOWN
     // лимиты в копейках
-    val dayCardLimitTransfer = 150_000_00
-    val monthCardLimitTransfer = 600_000_00
-    val beforeFee = 75_000_00
-    val vkPerOnceLimit = 15_000_00
-    val vkPerMonthLimit = 40_000_00
+//    val dayCardLimitTransfer = 150_000_00
+//    val monthCardLimitTransfer = 600_000_00
+//    val beforeFee = 75_000_00
+//    val vkPerOnceLimit = 15_000_00
+//    val vkPerMonthLimit = 40_000_00
 
 
     when {
         (typeCard == "MasterCard" || typeCard == "Maestro") -> return when {
-            (amountTransfer < dayCardLimitTransfer && amountTransfer < beforeFee && currentMonthTransfer < monthCardLimitTransfer &&
+            (amountTransfer < dayCardLimitTransfer
+                    && amountTransfer < beforeFee &&
+                    currentMonthTransfer < monthCardLimitTransfer &&
                     (amountTransfer + currentMonthTransfer) < monthCardLimitTransfer) -> "сумма перевода составит - ${amountTransfer / 100} руб без комиссии"
 
-            (amountTransfer < dayCardLimitTransfer && amountTransfer >= beforeFee && currentMonthTransfer < monthCardLimitTransfer &&
+            (amountTransfer < dayCardLimitTransfer &&
+                    amountTransfer >= beforeFee &&
+                    currentMonthTransfer < monthCardLimitTransfer &&
                     (amountTransfer + currentMonthTransfer) < monthCardLimitTransfer) -> "сумма перевода составит - ${
                 df.format(
                     (amountTransfer * 1.006 + 2000) / 100
@@ -45,7 +54,8 @@ fun calculate(typeCard :String = "VK Pay", currentMonthTransfer: Double = 0.0, a
         }
 
         (typeCard == "Visa" || typeCard == "Мир") -> return when {
-            (amountTransfer < dayCardLimitTransfer && currentMonthTransfer < monthCardLimitTransfer &&
+            (amountTransfer < dayCardLimitTransfer &&
+                    currentMonthTransfer < monthCardLimitTransfer &&
                     (amountTransfer + currentMonthTransfer) < monthCardLimitTransfer) -> when {
                 (amountTransfer * 0.0075) < 3500 -> "сумма перевода составит - ${(amountTransfer + 3500) / 100}руб, комиссия - 35 руб"
                 else -> "сумма перевода составит - ${df.format((amountTransfer * 1.0075) / 100)}руб, комиссия - ${
@@ -60,7 +70,9 @@ fun calculate(typeCard :String = "VK Pay", currentMonthTransfer: Double = 0.0, a
         }
 
         else -> return when {
-            (amountTransfer < vkPerOnceLimit && currentMonthTransfer < vkPerMonthLimit && (amountTransfer + currentMonthTransfer) < vkPerMonthLimit) ->
+            (amountTransfer < dayCardLimitTransfer &&
+                    currentMonthTransfer < monthCardLimitTransfer
+                    && (amountTransfer + currentMonthTransfer) < monthCardLimitTransfer) ->
                 "сумма перевода составит - ${amountTransfer / 100} без комиссии"
 
             else -> "Вы превысили лимиты! Перевод не осуществлён!"
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -19,7 +19,7 @@ class MainKtTest {
         assertEquals("сумма перевода составит - 13900.0 без комиссии", result)
     }
     @Test
-    fun calculateVKLimits() {
+    fun calculateVKLimitsDay() {
         val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.DOWN
         val  currentMonthTransfer = 10_000_00.00
@@ -29,15 +29,68 @@ class MainKtTest {
         assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
     }
     @Test
+    fun calculateVKLimitsMonth() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 45_000_00.00
+        val amountTransfer = 13_900_00.00
+
+        val result = calculate(currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
+    }
+    @Test
+    fun calculateVKLimitsDayPlusMonth() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 10_000_00.00
+        val amountTransfer = 38_900_00.00
+
+        val result = calculate(currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
+    }
+    @Test
+    fun calculateVKDayPlusMonth() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 10_000_00.00
+        val amountTransfer = 8_900_00.00
+
+        val result = calculate(currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        assertEquals("сумма перевода составит - 8900.0 без комиссии", result)
+    }
+    @Test
     fun calculateMasterWithFee() {
         val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.DOWN
         val  currentMonthTransfer = 500_000_00.00
         val amountTransfer = 83_900_00.00
         val card = "MasterCard"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val beforeFee = 75_000_00.00
 
-        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer ,
+            amountTransfer = amountTransfer, dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee)
         assertEquals("сумма перевода составит - 84423,4руб, комиссия - 523,4 руб", result)
+
+
+    }
+    @Test
+    fun calculateMasterWithFeeMin() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 500_000_00.00
+        val amountTransfer = 75_000_00.00
+        val card = "MasterCard"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val beforeFee = 75_000_00.00
+
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer ,
+            amountTransfer = amountTransfer, dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee)
+        assertEquals("сумма перевода составит - 75470руб, комиссия - 470 руб", result)
 
 
     }
@@ -48,8 +101,13 @@ class MainKtTest {
         val  currentMonthTransfer = 500_000_00.00
         val amountTransfer = 72_900_00.00
         val card = "MasterCard"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val beforeFee = 75_000_00.00
 
-        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee)
         assertEquals("сумма перевода составит - 72900.0 руб без комиссии", result)
 
 
@@ -61,8 +119,29 @@ class MainKtTest {
         val  currentMonthTransfer = 500_000_00.00
         val amountTransfer = 100_900_00.00
         val card = "MasterCard"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val beforeFee = 75_000_00.00
 
-        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee)
+        assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
+    }
+    @Test
+    fun calculateMasterDayLimit() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 500_000_00.00
+        val amountTransfer = 160_900_00.00
+        val card = "MasterCard"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val beforeFee = 75_000_00.00
+
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer,
+            beforeFee = beforeFee)
         assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
 
 
@@ -74,9 +153,24 @@ class MainKtTest {
         val  currentMonthTransfer = 500_000_00.00
         val amountTransfer = 13_900_00.00
         val card = "Visa"
-
-        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer)
         assertEquals("сумма перевода составит - 14004,25руб, комиссия - 104,25 руб", result)
+    }
+    @Test
+    fun calculateVisaMinFee() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 500_000_00.00
+        val amountTransfer = 900_00.00
+        val card = "Visa"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer)
+        assertEquals("сумма перевода составит - 935.0руб, комиссия - 35 руб", result)
 
 
     }
@@ -87,10 +181,23 @@ class MainKtTest {
         val  currentMonthTransfer = 100_000_00.00
         val amountTransfer = 163_900_00.00
         val card = "Visa"
-
-        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer)
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer)
         assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
-
-
+    }
+    @Test
+    fun calculateVisaLimitsMonth() {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val  currentMonthTransfer = 600_000_00.00
+        val amountTransfer = 13_900_00.00
+        val card = "Visa"
+        val dayCardLimitTransfer = 150_000_00.00
+        val monthCardLimitTransfer = 600_000_00.00
+        val result = calculate(typeCard = card, currentMonthTransfer = currentMonthTransfer , amountTransfer = amountTransfer,
+            dayCardLimitTransfer = dayCardLimitTransfer, monthCardLimitTransfer = monthCardLimitTransfer)
+        assertEquals("Вы превысили лимиты! Перевод не осуществлён!", result)
     }
 }
